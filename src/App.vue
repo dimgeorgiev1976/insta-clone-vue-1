d2d20e229e78e4a21e13fdca20117851de723c16
Алексей Данчин <aleksey.danchin@gmail.com>
2019-12-17 19:43:35 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+        <div class="container">
+            <div class="header__row">
+
+                <a href="index.html" class="header__logo">
+                    <i class="fab fa-dev"></i>
+                </a>
+
+                <div class="header__search">
+                    <div class="search">
+                        <input type="text" placeholder="Поиск">
+                        <div class="search__cancel search__cancel--active">
+                            <i class="fas fa-times-circle"></i>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="header__links">
+                    <a href="#" class="header__link">
+                        <i class="fas fa-user"></i>
+                    </a>
+                    <a href="#" class="header__link">
+                        <i class="fas fa-paper-plane"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <post
+            v-for="post of posts"
+            :data="post"
+            v-bind:key="post.id"
+        ></post>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from '@/components/Post'
+import fakedata from './fakedata.json'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+
+    components: {
+        Post
+    },
+
+    data() {
+        return {
+            posts: fakedata.posts
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="post">
+
+        <div class="post__header">
+
+            <div class="post__user">
+                <div class="user">
+                    <a href="#" class="user__avatar">
+                        <img :src="data.user.ava" alt="">
+                    </a>
+                    <a href="#" class="user__name">{{ data.user.name }} {{ data.user.surname }}</a>
+                </div>
+
+            </div>
+
+            <div class="post__links">
+                <a href="#" class="edit-link">
+                    <i class="far fa-trash-alt"></i>
+                    Delete
+                </a>
+
+
+                <a href="edit.html" class="edit-link">
+                    <i class="fas fa-pencil-alt"></i>
+                    Edit
+                </a>
+
+            </div>
+        </div>
+
+        <div class="post__img">
+            <img :src="data.src" alt="Photo">
+        </div>
+
+        <!-- post__data  -->
+        <div class="post__data">
+
+            <div class="post__description">{{ data.description }}</div>
+
+            <div class="post__hashtags">
+                <template v-for="tag of data.tags">
+                    <a href="#" v-bind:key="tag">#{{ tag }}</a>
+                </template>
+            </div>
+        </div>
+        <!-- // post__data -->
+
+        <!-- post__comments -->
+        <div class="post__comments">
+            <Comment v-for="comment of data.comments" :data="comment" v-bind:key="comment.id"></Comment>
+        </div>
+        <!-- // post__comments -->
+
+        <div class="post__comment-form">
+            <div class="comment-form">
+                <textarea class="comment-form__textarea" placeholder="Добавить комментарий"></textarea>
+                <button disabled class="comment-form__button">Опубликовать</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import Comment from '@/components/Comment'
+
+export default {
+    props: ['data'],
+    components: {
+        Comment
+    }
+}
+</script>
