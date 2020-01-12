69a972fec71464e57034422861c9b356c7222b84
dimgeorgiev1976 <dim.georgiev1976@gmail.com>
2020-01-12 14:19:21 +0000

    Допилил router

--- back/router/index.js
+++ back/router/index.js
@@ -5,11 +5,19 @@ import comment from './comment'
 import post from './post'
 import wall from './wall'
 
+import fs from 'fs'
+import path from 'path'
 const router = new KoaRouter
 
 router.use('/api/account', account.routes(), account.allowedMethods())
 router.use('/api/comment', comment.routes(), comment.allowedMethods())
 router.use('/api/post', post.routes(), post.allowedMethods())
 router.use('/api/wall', wall.routes(), wall.allowedMethods())
+router.get('*', asunc (ctx, next) => {
+	const fullPath = path.join(__dirname, '../../front/dist/index.html')
+	ctx.set("content.type", "text/html; charset=utf-8")
+	ctx.body = fs.readFileSync(fullPath)
 
+})
+	
 export default router
